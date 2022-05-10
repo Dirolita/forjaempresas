d5f065e5d9523bb1b2ffe2bd6a8cdb6651a0dfe7
DianaCaro <dianacaroserranot@gmail.com>
2022-05-10 13:05:16 -0500

    modal js and css

--- js/script.js
+++ js/script.js
@@ -1,35 +1,46 @@
 
-
-/*let boton=document.getElementById("boton");
-let alertUser=document.getElementById("alertUser");
-
-    boton.addEventListener("click", ()=>{
+let jsboton=document.getElementById("boton");
+if(jsboton != null){
+    jsboton.addEventListener("click", ()=>{
+        
+        let alertUser=document.getElementById("alertUser");
         let user=document.getElementById("user").value;
         let psw=document.getElementById("psw").value;
         if(user==="Admin" && psw==="Kuepa2022"){
-           window.location = "dashboard.html";
+            window.location = "dashboard.html";
         }
         else{
             alertUser.style.display="block";
     }
     });
 
+
  let btnOk=document.getElementById("btnOk");
  btnOk.addEventListener("click",()=>{
     alertUser.style.display="none";
- });*/
+ });
+}
 
  //dashboard
+ let form=document.getElementById("formConsul");
 
- var form=document.getElementById("formConsul");
- //var consul = parseFloat(document.getElementById("consulta").value);
- //console.log(consul);
- form.addEventListener("submit",(e)=>{
+form.addEventListener("submit",(e)=>{
+
     var consul = parseInt(document.getElementById("consulta").value);
     console.log(consul);
-     e.preventDefault();
-     if(consul == 25413574){
-         alert("h")
+    e.preventDefault();
+
+    if(consul == 52368795){
+    alert("tiene la primera dosis");
      }
-     }
+
+    else if (consul == 1001235478){
+    alert("tiene primer y segunda dosis");
+    }
+    
+    else if (consul == 25413574){
+        alert("sin registro");
+        }
+    }
  );
+
